b885980cbd4808087fcb42de2bf6916cbe808472
Vinil <vinilsmonteiro@gmail.com>
2025-11-28 16:25:33 +0530

    clss and object

---
+++ src/Car.java
@@ -0,0 +1,25 @@
+public class Car {
+    String color;
+    int speed;
+
+    public void setSpeed(int speed){
+        if(speed >= 0){
+            this.speed = speed;
+        }else{
+            System.out.println("Invalid Speed");
+        }
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+    public void setColor(String color){
+        this.color  = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+
+}
